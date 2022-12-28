cd48f6aaf349463a929a846e60d75172dd5f0cec
pavanprostack <pavanprostack@gmail.com>
2022-12-28 14:42:56 +0530

    basic

---
+++ basics/datatype-crud.py
@@ -0,0 +1,13 @@
+id=101
+name = "rahul"
+
+# print() is a output function
+print(id);
+print(name);
+
+# To find data type.
+print(type(id))
+print(type(name))
+
+# To find address of object.
+print(id(name))
