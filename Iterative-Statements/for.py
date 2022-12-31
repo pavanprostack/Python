0381f5eb76cfc8e05d3c0b7431a32f4f6ad6286e
pavanprostack <pavanprostack@gmail.com>
2022-12-31 16:06:43 +0530

    iterative statements

--- Iterative-Statements/for.py
+++ Iterative-Statements/for.py
@@ -3,19 +3,27 @@
 for x in range(11):
     print(x)
     
-
+print("*****")
     
 # to print 2,3,4,5,....18.
 for y in range(2, 19):
     print(y)
+
+print("*****")
     
-    
-# to print even numbers 2,4,6.....20(increment).
+# to print even numbers 2,4,6.....20(increment 2).
 for a in range(2, 21, 2):
     print(a)
+
+print("*****")
     
-    
-# to print event numbers 20,18,16,14......0(decrement).
+# to print event numbers 20,18,16,14......0(decrement 2).
 for b in range(20, -1, -2):
+    print(b)
+    
+print("*****")
+
+# to print odd numbers 1,3,5,7......19 (increment).
+for b in range(1, 20, 2) :
     print(b)
     
